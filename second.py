c2ec3b175cb1b0111a9e834920daa2b7d170d146
rohith <rohithmidde9@gmail>
2024-08-07 06:36:38 +0530

    completion

--- second.py
+++ second.py
@@ -9,19 +9,22 @@ print(f"length is {length}")
 cond=False
 lives=len(word)
 while not cond:
+    count=0
     letter=input("enter letter:")
     for i in range(0,len(word)):
         search=word[i]
         if search==letter:
             length[i]=letter
             print(length)
-    if letter not in word:
+            count=1
+    if count==0:
         lives-=1
         if lives==0:
             cond=True
             print("you lose")
     if "_" not in length:
         print("you win")
+        cond=True
     
 
 

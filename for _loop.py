d99d00f8e39fd6f91c4655f07dbbe94551efbacb
rohith <rohithmidde9@gmail>
2024-08-07 22:40:06 +0530

    for loop

---
+++ for _loop.py
@@ -0,0 +1,11 @@
+word=input("enter word:")
+while True:
+    count=0
+    guess=input("enter letter:")
+    for i in range(0,len(word)):
+        if (guess==word[i]):
+           count+=1
+    if count>0:
+        print("letter is in word")
+    else:
+        print("letter is not in word")
